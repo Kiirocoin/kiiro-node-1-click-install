2033f09ac5cd54d6825c0e756698bbdfb126b78e
Kiirodev <142940674+Kiirodev@users.noreply.github.com>
2023-09-13 17:16:27 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Check if user is root
-if [ "$EUID" -ne 0 ]
-  then banner; echo "Please run as root"
-  exit
-fi
 
 function banner {
 orange='\033[0;33m'
@@ -14,103 +9,69 @@ printf "${orange}
 █████   ██ ██ ██████  ██    ██ ██      ██    ██ ██ ██ ██  ██ 
 ██  ██  ██ ██ ██   ██ ██    ██ ██      ██    ██ ██ ██  ██ ██ 
 ██   ██ ██ ██ ██   ██  ██████   ██████  ██████  ██ ██   ████"
-echo ""
+printf ""
 printf "
 ███    ███  █████  ███████ ████████ ███████ ██████  ███    ██  ██████  ██████  ███████ 
 ████  ████ ██   ██ ██         ██    ██      ██   ██ ████   ██ ██    ██ ██   ██ ██      
 ██ ████ ██ ███████ ███████    ██    █████   ██████  ██ ██  ██ ██    ██ ██   ██ █████   
 ██  ██  ██ ██   ██      ██    ██    ██      ██   ██ ██  ██ ██ ██    ██ ██   ██ ██      
 ██      ██ ██   ██ ███████    ██    ███████ ██   ██ ██   ████  ██████  ██████  ███████"
-echo ""
+printf ""
 printf "
 ██ ███    ██ ███████ ████████  █████  ██      ██      ███████ ██████  
 ██ ████   ██ ██         ██    ██   ██ ██      ██      ██      ██   ██ 
 ██ ██ ██  ██ ███████    ██    ███████ ██      ██      █████   ██████  
 ██ ██  ██ ██      ██    ██    ██   ██ ██      ██      ██      ██   ██ 
 ██ ██   ████ ███████    ██    ██   ██ ███████ ███████ ███████ ██   ██"
-echo ""
+printf ""
 sleep 2
 }
-banner
-#vpsIp=`ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
-echo ""
-#echo "Please enter VPS IP address: ${vpsIp} (Press enter if this is correct)"
-echo "Please enter VPS IP address only: example 123.456.789.012 - DO NOT ADD PORT"
-read vpsIp
-#if [ -z "$vpsIp" ]
-#then
-#      vpsIp=`ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
-#fi
-echo "Please enter BLS generated Secret: "
-read blsSecret
-echo ""
-echo ""
-echo "***************************"
-echo "Updating & Installing unzip"
-echo "***************************"
-echo ""
-sudo apt update && sudo apt-get install unzip -f
-echo "Downloading ubuntu-18 Kiiro wallet"
-sudo rm -f ubuntu-18.zip && wget https://github.com/Kiirocoin/kiiro/releases/download/v1.0.0.3/ubuntu-18.zip
-echo ""
-echo "***************************"
-echo "Done"
-echo "***************************"
-banner
-echo ""
-echo "***************************"
-echo "Unzipping"
-echo "***************************"
-echo ""
-unzip -o ubuntu-18.zip
-echo ""
-echo "***************************"
-echo "Done"
-echo "***************************"
-banner
-echo ""
-echo "***************************"
-echo "Moving files to /usr/bin"
-echo "***************************"
-echo ""
-sudo mv -f ubuntu-18/kiirocoin-cli /usr/bin && sudo mv -f ubuntu-18/kiirocoind /usr/bin
-echo ""
-echo "***************************"
-echo "Done"
-echo "***************************"
-banner
-echo ""
-echo "***************************"
-echo "Chmod files as executable"
-echo "***************************"
-echo ""
-chmod +x /usr/bin/kiirocoin-cli && chmod +x /usr/bin/kiirocoind
-echo ""
-echo "***************************"
-echo "Done"
-echo "***************************"
-banner
-echo ""
-echo "**********************************"
-echo "Creating directory for node files"
-echo "**********************************"
-echo ""
-mkdir -p ~/.kiirocoin
-echo ""
-echo "***************************"
-echo "Done"
-echo "***************************"
-banner
 
-echo ""
-echo "*******************************"
-echo "Making kiirocoind service file"
-echo "*******************************"
-echo ""
+hr="\n\n****************************************************************************************\n\n";
+# Check if user is root
+if [ "$EUID" -ne 0 ]
+  then banner; printf ""; root=0; printf "${hr}WARNING${hr}"; printf ""; printf "User is not root.\n\n"; printf "Script may fail with some operations if current user is not in sudo group or configured correctly.";sleep 4;
+fi
+banner
+printf "${hr}"
+vpsIp=`hostname -I`
+
+printf "Your VPS IP = ${vpsIp}\n\n"
+printf "If your VPS IP is not correct or is blank, please contact Kiirocoin Support team for assistance with editing kiirocoin.conf after this install\n\n"
+printf "Please enter BLS generated Secret: \n"
+read blsSecret
+length=`expr length ${blsSecret//[[:blank:]]/}`;
+if [ $length != 64 ];
+then printf "\n\nblsSecret is not valid. Please restart script and enter a valid blsSecret.\n\n";exit;fi;
+printf "${hr}Updating & Installing unzip${hr}"
+sudo apt update && sudo apt-get install unzip -f
+printf "Downloading ubuntu-18 Kiiro wallet"
+sudo rm -f ubuntu-18.zip && wget https://github.com/Kiirocoin/kiiro/releases/download/v1.0.0.3/ubuntu-18.zip
+printf "${hr}Done${hr}"
+banner
+printf "${hr}Unzipping${hr}"
+unzip -o ubuntu-18.zip
+printf "${hr}Done${hr}"
+banner
+printf "${hr}Moving files to /usr/bin${hr}"
+sudo mv -f ubuntu-18/kiirocoin-cli /usr/bin && sudo mv -f ubuntu-18/kiirocoind /usr/bin
+printf "${hr}Done${hr}"
+banner
+printf "${hr}Chmod files as executable${hr}"
+chmod +x /usr/bin/kiirocoin-cli && chmod +x /usr/bin/kiirocoind
+printf "${hr}Done${hr}"
+banner
+printf "${hr}Creating kiirocoind service file${hr}"
+sudo systemctl disable kiirocoind
+sudo rm /etc/systemd/system/kiirocoind.service
 rpcuser=`tr -dc A-Za-z0-9 </dev/urandom | head -c 12`
 rpcpassword=`tr -dc A-Za-z0-9 </dev/urandom | head -c 12`
 
-cat <<EOT > /etc/systemd/system/kiirocoind.service
+#if [ $root=0 ];
+#then FILE_NAME="/home/$USER/.kiirocoin/kiirocoind.pid";
+#else FILE_NAME="/root/.kiirocoin/kiirocoind.pid";
+#fi
+cat <<EOF > /etc/systemd/system/kiirocoind.service
 # Install this in /etc/systemd/system/
 # See below for more details and options
 # Then run following to always start:
@@ -155,54 +116,28 @@ MemoryDenyWriteExecute=true
 
 [Install]
 WantedBy=multi-user.target
-EOT
-echo ""
-echo "***************************"
-echo "Done"
-echo "***************************"
+EOF
+
+
+printf "${hr}Done${hr}"
 banner
-echo ""
-echo "**********************************"
-echo "Enabling kiirocoind service"
-echo "**********************************"
-echo ""
+printf "${hr}Enabling kiirocoind service${hr}"
 sudo systemctl enable kiirocoind
-echo ""
-echo "***************************"
-echo "Done"
-echo "***************************"
+printf "${hr}Done${hr}"
 banner
-echo ""
-echo "**********************************"
-echo "Setting and Enabling Firewall"
-echo "**********************************"
-echo ""
-apt install -y ufw && ufw allow ssh/tcp && ufw limit ssh/tcp && ufw allow 8999/tcp && ufw logging on && ufw --force enable
-echo ""
-echo "***************************"
-echo "Done"
-echo "***************************"
+printf "${hr}Configuring and Enabling Firewall${hr}"
+sudo apt install -y ufw && sudo ufw allow ssh/tcp && sudo ufw limit ssh/tcp && sudo ufw allow 8999/tcp && sudo ufw logging on && sudo ufw --force enable
+printf "${hr}Done${hr}"
 banner
-echo ""
-echo "**********************************"
-echo "Creating directory for node files"
-echo "**********************************"
-echo ""
-mkdir -p ~/.kiirocoin
-echo ""
-echo "***************************"
-echo "Done"
-echo "***************************"
+printf "${hr}Creating directory for Masternode files${hr}"
+mkdir -p /root/.kiirocoin
+printf "${hr}Done${hr}"
 banner
-echo ""
-echo "***************************"
-echo "Making kiirocoin.conf file"
-echo "***************************"
-echo ""
+printf "${hr}Making kiirocoin.conf file${hr}"
+sudo rm /root/.kiirocoin/kiirocoin.conf
 rpcuser=`tr -dc A-Za-z0-9 </dev/urandom | head -c 12`
 rpcpassword=`tr -dc A-Za-z0-9 </dev/urandom | head -c 12`
-
-cat <<EOT > ~/.kiirocoin/kiirocoin.conf
+cat <<EOT > /root/.kiirocoin/kiirocoin.conf
 #----
 rpcuser=${rpcuser}
 rpcpassword=${rpcpassword}
@@ -217,42 +152,25 @@ logtimestamps=1
 txindex=1
 #----
 znode=1
-externalip=${vpsIp}:8999
-znodeblsprivkey=${blsSecret}
+externalip=${vpsIp//[[:blank:]]/}:8999
+znodeblsprivkey=${blsSecret//[[:blank:]]/}
 EOT
-echo ""
-echo "***************************"
-echo "Done"
-echo "***************************"
+
+printf "${hr}Done${hr}"
 banner
-echo ""
-echo "***************************"
-echo "Removing old files"
-echo "***************************"
-echo ""
+printf "${hr}Removing old files${hr}"
 sudo rm -rd ubuntu-18 && sudo rm ubuntu-18.zip*
-echo ""
-echo "***************************"
-echo "Done"
-echo "***************************"
+printf "${hr}Done${hr}"
 banner
-echo ""
-echo "Install of Masternode is complete."
-echo ""
-echo "Starting Masternode and waiting 10 seconds"
+printf "${hr}Install of Masternode is complete.\n\n"
+printf "Starting Masternode and waiting 15 seconds\n\n"
 #kiirocoind -daemon
-systemctl start kiirocoind
-echo ""
-sleep 10
-echo "Running kiirocoin-cli evoznode status"
+sudo systemctl start kiirocoind
+sleep 15
+printf "Running kiirocoin-cli evoznode status\n\n"
 kiirocoin-cli evoznode status
-echo ""
-echo "If you do not see ready then run the following command again:"
-echo ""
-echo "kiirocoin-cli evoznode status"
-echo "" 
-echo "If you are receiving an error, you may have not properly registered Masternode or entered wrong information in beginning of install script"
-echo ""
-echo "Reach out to us on Discord if you need any help"
-echo ""
-echo "https://discord.gg/g88D2RP9"
+printf "\n\nIf you do not see ready then run the following command again in 30 minutes:\n\n"
+printf "kiirocoin-cli evoznode status\n\n"
+printf "If you are receiving an error, you may have not properly registered Masternode or entered wrong information in beginning of install script\n\n"
+printf "Reach out to us on Discord if you need any help\n\n"
+printf "https://discord.gg/g88D2RP9\n\n"
